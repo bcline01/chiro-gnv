e5526d1ea366c2bdc1ebadc46152361d06c1505f
bcline01 <brookecline.bc@gmail.com>
2025-03-15 21:54:56 -0400

    carousel working. needs polishing

--- src/components/OwlCarouselWrapper.tsx
+++ src/components/OwlCarouselWrapper.tsx
@@ -6,7 +6,9 @@ import Man1 from '../assets/man1.png';
 import Man2 from '../assets/man2.png';
 import Man3 from '../assets/man3.png';
 import Man4 from '../assets/doc-4.png'; 
+import { doctors } from './doctors';
 import '../styles/OwlCarouselWrapper.css';
+import { Link } from 'react-router-dom';
 
 // Options for react-slick carousel
 const options = {
@@ -22,48 +24,24 @@ const options = {
 const OwlCarouselWrapper: React.FC = () => {
   return (
     <Slider {...options}>
-      <div className="carousel-item">
-        <div className="doc-item">
-          <img 
-            src={Man1} 
-            alt="Dr. Matthew Richeson" 
-          />
-          <h3>Dr. Matthew Richeson</h3>
-          {/* <p>Chiropractor and Wellness Specialist</p> */}
+      {doctors.map((doctor) => (
+        <div className="carousel-item" key={doctor.id}>
+          <div className="doc-item">
+            <img 
+              src={doctor.photo} 
+              alt={doctor.name} 
+              className="doc-image" 
+            />
+            <h3>{doctor.name}</h3>
+            <Link to={`/doctors/${doctor.id}`} className="learn-more-doc">
+              Learn More
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="carousel-item">
-        <div className="doc-item">
-          <img 
-            src={Man2} 
-            alt="Dr. Matthew Cline" 
-          />
-          <h3>Dr. Matthew Cline</h3>
-          {/* <p>Chiropractor and Wellness Specialist</p> */}
-        </div>
-      </div>
-      <div className="carousel-item">
-        <div className="doc-item">
-          <img 
-            src={Man3} 
-            alt="Dr. Cooper Cline" 
-          />
-          <h3>Dr. Cooper Cline</h3>
-          {/* <p>Chiropractor and Wellness Specialist</p> */}
-        </div>
-      </div>
-      <div className="carousel-item">
-        <div className="doc-item">
-          <img 
-            src={Man4} 
-            alt="Dr. Kent Owens" 
-          />
-          <h3>Dr. Kent Owens</h3>
-          {/* <p>Chiropractor and Wellness Specialist</p> */}
-        </div>
-      </div>
+      ))}
     </Slider>
   );
 };
+
 
 export default OwlCarouselWrapper;
